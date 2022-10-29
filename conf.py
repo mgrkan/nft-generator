22648a8b24c1d6dde722025578fdb98749528f1c
mgrkan <fogrkan@gmail.com>
2022-10-29 23:43:09 +0300

    change in test resolution

--- conf.py
+++ conf.py
@@ -2,4 +2,4 @@ import os
 
 PROJECT_NAME = "Sample" #Name of the nft
 ASSETS = 10 #Asset amount as an integer
-RESOLUTION = (256,256) # h, w values as a tuple
+RESOLUTION = (800,800) # h, w values as a tuple
