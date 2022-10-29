fbe6780297d488f92106e65e9ba9b8b084dd7ed4
mgrkan <fogrkan@gmail.com>
2022-10-30 00:15:12 +0300

    added more config

--- conf.py
+++ conf.py
@@ -1,5 +1,7 @@
-import os
 
 PROJECT_NAME = "Sample" #Name of the nft
-ASSETS = 10 #Asset amount as an integer
+ASSETS = 42 #Asset amount as an integer
+STARTS_FROM_ZERO = False #If the asset name starts from 0
+#Note that if you set it to True the last asset will be ASSETS - 1
+FIXED_DECIMALS = 4 #Amount of decimals in the name: Sample#0001
 RESOLUTION = (800,800) # h, w values as a tuple
