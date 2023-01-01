176bf3cbbae13e6a1aca781bdeee395bcc688b90
mgrkan <fogrkan@gmail.com>
2023-01-02 01:01:45 +0300

    dumping traits as json

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 import conf
 import random
 from rarities import rarities, check_rarity
+import json
 
 
 os.mkdir("./Output")
@@ -25,36 +26,44 @@ def naming(index):
     else:
         return conf.PROJECT_NAME + "#" +  str(index) + ".jpeg"
 
-trait_list = []
+trait_check = []
 exceeds_rarity = conf.LAYERS
 print(exceeds_rarity)
+trait_list = {}
 img = Image.new("RGB", res , color)
 global a
 a = start
 while a <= conf.ASSETS:
+    traits = []
     for i in layers:
         y = os.listdir("./Layers/" + i)
         selection = random.choice(y)
         trait_location = "./Layers/" + i + "/" + selection
         try:
             val = rarities.get(trait_location)
-            amount = check_rarity(conf.ASSETS, trait_location, val, trait_list, a)
+            amount = check_rarity(conf.ASSETS, trait_location, val, trait_check, a)
             print(trait_location,"rarity:", val,"amount:", amount)
-            if amount <= trait_list.count(trait_location):
+            if amount <= trait_check.count(trait_location):
                 exceeds_rarity[int(i)].append(trait_location)
                 print("exceeded rarity")
                 print(exceeds_rarity)
                 y.remove(selection)
                 new_selection = random.choice(y)
                 trait_location = "./Layers/" + i + "/" + new_selection
-
+                traits.append(new_selection.replace(".png", ""))
+            else:
+                traits.append(selection.replace(".png", ""))
+                
         except:
+            traits.append(selection.replace(".png", ""))
             pass
 
-        trait_list.append(trait_location)
+        trait_check.append(trait_location)
         trait_location = Image.open(trait_location)
         img.paste(trait_location, (0,0), mask=trait_location )
-            
+    trait_list[naming(a).replace(".jpeg", "")] = traits
     img.save(os.path.join(output_dir, naming(a)))
     print(naming(a))
     a += 1
+with open("traits.json", "w") as f:
+    f.write(json.dumps(trait_list))
