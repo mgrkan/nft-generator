2287b0b5047c456c17f8b31ae40256d80b47dfae
Muharrem Gürkan Cengiz <79905545+mgrkan@users.noreply.github.com>
2023-03-23 05:18:32 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,8 +3,8 @@ PROJECT_NAME = "Sample" #Name of the nft
 ASSETS = 100 #Asset amount as an integer
 STARTS_FROM_ZERO = False #If the asset name starts from 0
 #Note that if you set it to True the last asset will be ASSETS - 1
-FIXED_DECIMALS = 4 #Amount of decimals in the name: Sample#0001
-#If you dont want additional decimals set it to 0
+FIXED_DIGITS = 4 #Amount of digits in the name: Sample#0001
+#If you dont want additional digits set it to 0
 RESOLUTION = (800,800) # h, w values as a tuple
 EXTENSION = ".jpeg"
 LAYERS = {0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[]}
